ed1b7f5f3ce9a58185ed3258eb48392272221623
Archana <archanasnaik2017@gmail.com>
2022-06-11 13:04:18 +0530

    Created login page

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import Input from "../Components/Input";
+import Button from "../Components/Button";
 import "./Login.css";
 
 const Login = () => {
+    const LoginHandler = () => {
+        return
+    }
+    const RegisterHandler = () => {
+        return
+    }
   return (
     <>
       <div className="main_box">
         <div className="log_box">
+          <h2 className="Heading">Sign In</h2>
           <div className="form-input">
-            <h2 className="heading">Sign In</h2>
             <Input class="user" label="User Name" />
             <Input class="password" label="Password" />
+          </div>
+          <div className="btn-box">
+            <Button class="login-styl" name="Login" onClick={LoginHandler}/>
+            <Button class="regi-styl" name="Register" onClick={RegisterHandler} />
           </div>
         </div>
       </div>
